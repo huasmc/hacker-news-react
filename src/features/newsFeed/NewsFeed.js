0a5b0d50c24b00b75c771be188ccc78998a2e962
huasmc <huasmc@gmail.com>
2023-02-09 20:09:42 -0400

    refactor: pagination

--- src/features/newsFeed/NewsFeed.js
+++ src/features/newsFeed/NewsFeed.js
@@ -1,25 +1,10 @@
 import { memo } from "react";
-import { useSelector } from "react-redux";
 import { Col, Row } from "../../common/styled";
-import { selectPages } from "../dashboard/DashboardSlice";
 import NewsFeedItem from "./components/NewsFeedItem";
-import {
-	StyledPaginationButton,
-	StyledPaginationButtonActive,
-} from "./components/style";
+import NewsFeedPagination from "./components/NewsFeedPagination";
 
 const NewsFeed = ({ news, page, setPage }) => {
-	const numberOfPages = useSelector(selectPages);
-
 	const selectPage = (page) => setPage(page);
-
-	const previousPage = () => {
-		if (page > 0) setPage(page - 1);
-	};
-
-	const nextPage = () => {
-		if (page < numberOfPages) setPage(page + 1);
-	};
 
 	return (
 		<div>
@@ -38,35 +23,7 @@ const NewsFeed = ({ news, page, setPage }) => {
 				</Col>
 			</Row>
 			<Row>
-				<StyledPaginationButton onClick={previousPage}>
-					<span style={{ fontWeight: "700", cursor: "pointer" }}>{"<"}</span>
-				</StyledPaginationButton>
-				{Array.from(Array(numberOfPages).keys())
-					.slice(page, page + 9)
-					.map((pageNumber) => {
-						return (
-							<>
-								{page !== pageNumber ? (
-									<StyledPaginationButton
-										key={pageNumber}
-										onClick={() => selectPage(pageNumber)}
-									>
-										{pageNumber + 1}
-									</StyledPaginationButton>
-								) : (
-									<StyledPaginationButtonActive
-										key={pageNumber}
-										onClick={() => selectPage(pageNumber)}
-									>
-										{pageNumber + 1}
-									</StyledPaginationButtonActive>
-								)}
-							</>
-						);
-					})}
-				<StyledPaginationButton onClick={nextPage}>
-					<span style={{ fontWeight: "700", cursor: "pointer" }}>{">"}</span>
-				</StyledPaginationButton>
+				<NewsFeedPagination page={page} selectPage={selectPage} />
 			</Row>
 		</div>
 	);
---
+++ src/features/newsFeed/components/NewsFeedPagination.js
@@ -0,0 +1,52 @@
+import { StyledPaginationButton, StyledPaginationButtonActive } from "./style";
+import { memo } from "react";
+import { useSelector } from "react-redux";
+import { selectPages } from "../../dashboard/DashboardSlice";
+
+const NewsFeedPagination = ({ page, selectPage }) => {
+	const numberOfPages = useSelector(selectPages);
+
+	const previousPage = () => {
+		if (page > 0) selectPage(page - 1);
+	};
+
+	const nextPage = () => {
+		if (page < numberOfPages) selectPage(page + 1);
+	};
+
+	return (
+		<>
+			<StyledPaginationButton onClick={previousPage}>
+				<span style={{ fontWeight: "700", cursor: "pointer" }}>{"<"}</span>
+			</StyledPaginationButton>
+			{Array.from(Array(numberOfPages).keys())
+				.slice(page, page + 9)
+				.map((pageNumber) => {
+					return (
+						<>
+							{page !== pageNumber ? (
+								<StyledPaginationButton
+									key={pageNumber}
+									onClick={() => selectPage(pageNumber)}
+								>
+									{pageNumber + 1}
+								</StyledPaginationButton>
+							) : (
+								<StyledPaginationButtonActive
+									key={pageNumber}
+									onClick={() => selectPage(pageNumber)}
+								>
+									{pageNumber + 1}
+								</StyledPaginationButtonActive>
+							)}
+						</>
+					);
+				})}
+			<StyledPaginationButton onClick={nextPage}>
+				<span style={{ fontWeight: "700", cursor: "pointer" }}>{">"}</span>
+			</StyledPaginationButton>
+		</>
+	);
+};
+
+export default memo(NewsFeedPagination);
